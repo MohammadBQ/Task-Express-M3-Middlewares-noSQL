de033fbfdec8bc4ab0aca5476fb9dd752ecdde53
mohammadbq <iiprxh@gmail.com>
2023-06-21 00:22:58 +0300

    done

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const postsRoutes = require('./api/posts/posts.routes');
-const connectDb = require('./database');
-
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+const multer = require("multer");
+const postsRoutes = require("./api/posts/posts.routes");
+const connectDb = require("./database");
+const notFoundHandler = require("./middleware/notFoundHandler");
+const { error } = require("console");
+const errorHandler = require("./middleware/errorHandler");
+const PORT = process.env.PORT || 8000
 connectDb();
 app.use(express.json());
-app.use('/posts', postsRoutes);
 
-app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+//routes
+app.use("/posts", postsRoutes);
+
+//middlewares
+app.use("/media/", express.static(path.join(__dirname, "media")));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+//listener
+app.listen(PORT, () => {
+  console.log("The application is running on localhost:8000");
 });
